ad778d0f03d1ab00ea856d0c1d8dfd65b6a5efea
Luis <lromeror@fiec.espol.edu.ec>
2023-06-26 20:58:29 -0500

    New Proyect

---
+++ Proyectos/JuegoDominoTarea/src/main/java/ec/edu/espol/juegodominotarea/JuegoDominoTarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.juegodominotarea;
+
+/**
+ *
+ * @author Jonanyu 11.1
+ */
+public class JuegoDominoTarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
