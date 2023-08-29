7ad2e7366bc6d0378a7fc36e46bd073e28b64d26
Luis <lromeror@fiec.espol.edu.ec>
2023-08-29 01:25:39 -0500

    Aprendiendo contenido DInamico en POO

--- Proyectos/Calc/src/main/java/ec/edu/espol/calc/App.java
+++ Proyectos/Calc/src/main/java/ec/edu/espol/calc/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("Calculadora"), 640, 480);//Se cambia para el nuestro fxml
         stage.setScene(scene);
         stage.show();
     }
--- Proyectos/Calc/src/main/java/module-info.java
+++ Proyectos/Calc/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ec.edu.espol.calc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens ec.edu.espol.calc to javafx.fxml;
     exports ec.edu.espol.calc;
